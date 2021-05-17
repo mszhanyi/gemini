aaabe04e4bd8a2e566efee84f5d2f06289af90e0
Yi Zhang <zhanyi@microsoft.com>
2021-05-16 23:07:46 -0700

    update README

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # gemini
+For the well know reason, Jupyter Notebook doesn't support python multiprocessing.
+Some functions, like pytorch dataloader, will trigger some issues in Notebook on Windows.
+The goal of gemini is to help users can run Jupyter Notebook on Windows as well as on Linux.
 
-It helps users can run Jupyter NOTEBOOK on Windows as well as on Linux.
+## Prerequisite
+
+[The WSL and one Linux distribution](https://docs.microsoft.com/en-us/windows/wsl/install-win10) should be be installed in your windows 10 machine.
+
+## How to use
+
+Fast.ai is fully built on Jupyter notebooks. 
+Let's use fastai as the example.
+1. cd d:\git, git clone https://github.com/fastai/fastai
+2. download gemini.ps1 and installdeps.ps1
+3. powershell gemini.ps1 -n fastai -d installdeps.ps1
+4. try fastai/nbs/08_vision.data.ipynb 
+
+`-d installdeps.ps1` is optional, it is only used when you want to run some additional commands, such as install packages.
+
+
+## Limitations
+Only WSL2 supports GPU and only Geforce Cards are supported.
+
+## Refs
+https://github.com/pytorch/pytorch/issues/51344
+https://github.com/mszhanyi/pymultiprocessdemo
+https://github.com/microsoft/WSL/issues/5546
+
+
+
--- gemini.ps1
+++ gemini.ps1
@@ -8,20 +8,18 @@
 $ErrorActionPreference = "Stop"
 
 param (
-	[switch]$help,          # show help message
-	[string]$name,          # virtual environment, mandantory.
+    [switch]$help, # show help message
+    [string]$name, # virtual environment, mandantory.
     [string]$dependfile     # run some custom commands in WSL, such as install packages, optional. 
 )
 
-if ($help)
-{
+if ($help) {
     write-host "gemini -n {virtual name} -d {addtional commands file}"
-	exit
+    exit
 }
 
-if (!($name))
-{
-	write-host "Please specify virtual environment name, -n {name}"
+if (!($name)) {
+    write-host "Please specify virtual environment name, -n {name}"
 }
 
 # install mini conda, jupyter and create virtual environment
@@ -31,14 +29,13 @@ bash -c -i "conda activate $name"
 bash -c -i "conda list | grep jupyter || conda install -c anaconda jupyter"
 
 # run addtional commands
-if ($dependfile)
-{
-	write-host $dependfile
-	& $dependfile
+if ($dependfile) {
+    write-host $dependfile
+    & $dependfile
 }
 
 # start jupyter server
 $ret = bash -c -i "jupyter notebook --no-browser --port=8080"
 
 $url = $ret | Select-String -Pattern '^http:\/\/\w+(\.\w+)*(:[0-9]+)?(\/.*)?$'
-start $url
+Start-Process $url
