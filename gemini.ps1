d7a310f6674de6e47bd8aff1c94d45041470c810
Yi Zhang <zhanyi@microsoft.com>
2021-05-17 00:34:43 -0700

    some improvements

--- gemini.ps1
+++ gemini.ps1
@@ -2,16 +2,15 @@
 # Author: Yi Zhang
 # Version: 0.1
 # 
-# Windows browser with Notebook in WSL.
+# Start Notebook in WSL.
 #
-
-$ErrorActionPreference = "Stop"
-
-param (
+param(
     [switch]$help, # show help message
     [string]$name, # virtual environment, mandantory.
     [string]$dependfile     # run some custom commands in WSL, such as install packages, optional. 
 )
+
+$ErrorActionPreference = "Stop"
 
 if ($help) {
     write-host "gemini -n {virtual name} -d {addtional commands file}"
@@ -23,19 +22,16 @@ if (!($name)) {
 }
 
 # install mini conda, jupyter and create virtual environment
-bash -c -i "conda --version || ( wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh && bash Miniconda3-latest-Linux-x86_64.sh && source $HOME/.bashrc )"
-bash -c -i "conda env list | grep $name || conda create -n $name python=3.8"
+bash -c -i "conda --version || ( curl --retry 3 -o $HOME/install_miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh && bash $HOME/install_miniconda.sh -b && source $HOME/miniconda3/bin/activate)"
+bash -c -i "conda env list | grep $name || conda create -n $name python=3.8 -y -q"
 bash -c -i "conda activate $name"
-bash -c -i "conda list | grep jupyter || conda install -c anaconda jupyter"
+bash -c -i "conda list | grep jupyter || conda install -c anaconda jupyter -y -q"
 
 # run addtional commands
 if ($dependfile) {
     write-host $dependfile
-    & $dependfile
+    & $PSScriptRoot\$dependfile
 }
 
 # start jupyter server
 $ret = bash -c -i "jupyter notebook --no-browser --port=8080"
-
-$url = $ret | Select-String -Pattern '^http:\/\/\w+(\.\w+)*(:[0-9]+)?(\/.*)?$'
-Start-Process $url
